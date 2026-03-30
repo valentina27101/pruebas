05209242bd7b3f703bcdbc3506c54c4f6dc8c176
valentina27101 <pachecovalentinap@gmail.com>
2026-03-30 00:49:28 -0500

    Create tuplas.py

---
+++ tuplas.py
@@ -0,0 +1,15 @@
+#tuplas
+
+my_tupla = (53, 'perro', True, 2.3)
+print(type(my_tupla))
+
+#posicion
+print(my_tupla[0])
+
+#.count (cuenta cuantas opciones son iguales)
+print(my_tupla.count(53))
+
+#.index (encuentra lo que necesitemos)
+print(my_tupla.index(2.3))
+
+#no son mutables
